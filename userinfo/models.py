61f63e2bf3341257ab5ad30c499b32be0a6589a1
qqqytyu <qqqytyu@yeah.net>
2016-09-04 20:09:12 +0800

    add user model

--- userinfo/models.py
+++ userinfo/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    usr = models.CharField(max_length=30, verbose_name=u'用户名', null=False)
+    pwd = models.CharField(max_length = 30 , verbose_name = u'密码' , null = False )
+    name = models.CharField(max_length = 30 , verbose_name = u'昵称' , null = True)
