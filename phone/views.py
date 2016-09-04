6b295716f79a41209a993419cf3b9f6e9733a6cb
zyy <276878337@qq.com>
2016-09-04 21:50:15 +0800

    add

--- phone/views.py
+++ phone/views.py
@@ -1,22 +1,57 @@
 #coding:utf-8
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+#from userinfo.views import login
+from userinfo.models import User
 
+
+# 主页
+def index(request):
+    if request.user.is_authenticated():
+        username = request.session.get('username', 'anybody')
+        return render_to_response('phone/index.html', {'username':username})
+    else:
+        #login(request)
+        pass
 
 # 创建房间
-def CreateRoom(request):
-    pass
-# 提交配置
+def create_room(request):
+    if 'join' in request.GET:
+        # 加入房间
+        pass
+    else:
+        message = u'加入失败，房间不存在'
+    return HttpResponse(message)
 
 # 加入游戏
+def join_game(request):
+    pass
 
 # 开始游戏
+def start(request):
+    pass
 
-# 现实自己身份
+# 获取角色
+def get_role(request):
+    pass
+
 # 杀人
-# 验人
-# 救人
-# 毒人
-# 昨夜情况
+def kill_people(request):
+    pass
 
-def index(request):
-    return render(request, 'home.html')
+# 验人
+def check_people(request):
+    pass
+
+# 救人
+def rescue(request):
+    pass
+
+# 毒人
+def poison_people(request):
+    pass
+
+
+# 昨夜情况
+def yesternight(request):
+    pass
--- wolfkiller/urls.py
+++ wolfkiller/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from phone import views as phone_views
 from wechat import views as wecha_views
@@ -22,5 +22,5 @@ from wechat import views as wecha_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # url(r'^wechat/', wecha_views.index),
-    url(r'^phone/', phone_views.index)
+    url(r'^phone/',include('phone.urls'))
 ]
